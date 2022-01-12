15ab43a4aa7264a522ced727fb5f9e33b6c48813
adlynasn <95998006+adlynasn@users.noreply.github.com>
2022-01-12 12:31:56 +0800

    Update User.java
    I dont know whats wrong with the code

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,9 @@
 package main.java;
 
-public class User {
+import static java.lang.String.format;
+import static main.java.User.productsList.getProductsListBySellerID;
+
+public class User{
 
     private String username;
     private String email;
@@ -17,4 +20,84 @@ public class User {
     private Product[] productsList;
     private String[] transactionsHistory;
     private String[] orderNotifications;
+    
+    public static void main(String[] args) {
+       StoredDB[] products = getProductsListBySellerID(1);
+       for (StoredDB i : getProductsListBySellerID(1))
+           System.out.println(i);
 }
+    
+class productsList extends StoredDB {
+    private int productID,stock, sellerID;
+    private double price;
+    private String productName;
+    
+    public productsList() {
+        productID = 0;
+        productName = "";
+        price = 0.0;
+        stock = 0;   
+    }
+
+        public int getProductID() {
+            return productID;
+        }
+
+        public int getStock() {
+            return stock;
+        }
+
+        public int getSellerID() {
+            return sellerID;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public String getProductName() {
+            return productName;
+        }
+        
+        @Override
+        public String toString() {
+            String details = "Product Name : " + productName;
+            details = details + "\nPrice : " + price;
+            details = details + "\nStock :  " + stock;
+            details = details + "\nProductID : " + productID;
+        return details;
+             
+        }
+        
+        public static StoredDB[] getProductsListBySellerID(int sellerID) {
+           String query = String.format("SELECT * FROM Product " +
+                        "WHERE sellerID = '%d'",
+                         sellerID);
+           
+            return Database.getDBObjects(query, Product.class, -1);
+        }
+        
+        public static String getPrimaryKey() {
+        return "sellerID";
+    }
+    
+
+        @Override
+        public String insertQuery() {
+            return String.format("INSERT INTO " + "Product (productId, productName, stock, price)" + 
+                                 "VALUES ('%d', '%s','%d','%f)", productID, productName,stock,price);
+        }
+
+        @Override
+        public String updateQuery() {
+            return String.format("UPDATE Product " + "SET productName ='%s', stock='%d', price = '%f'" +
+                                "WHERE productID = %d", productName, stock,price, productID);
+        }
+
+        @Override
+        public String deleteQuery() {
+            return "DELETE FROM Product " + "WHERE ProductID = "+ productID;
+        } 
+}
+}
+
